4f4d9df5e02e5c2798ab5d37ab2bda4f0ffb666e
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-11-21 01:52:02 +0800

    Edit & Tech Modal Components

--- src/components/layout/AddBtn.js
+++ src/components/layout/AddBtn.js
@@ -12,7 +12,7 @@ const AddBtn = () => {
                         <i className="material-icons">person</i>
                     </a>
                 </li> <li>
-                    <a href="#tech-modal" className="btn-floating red modal-trigger">
+                    <a href="#add-tech-modal" className="btn-floating red modal-trigger">
                         <i className="material-icons">person_add</i>
                     </a>
                 </li>
