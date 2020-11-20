c1eb7a6949f2f8e49668520a0c5ffa41e94c09b5
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-11-20 10:42:21 +0800

    Materialize Setup
    Materialize Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React, { useEffect } from "react";
+
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+
 import "./App.css";
 
-import "materialize-css/dist/css/materialize.min.css"
-
 const App = () => {
+  useEffect(() => {
+    //Init Materialize JS
+    M.AutoInit();
+  });
   return <div className="App">My App</div>;
 };
 
